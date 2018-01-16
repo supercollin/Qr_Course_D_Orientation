b51c05af616232e9d7ac06aaac4ac1db3dfd9b9c
Tristan K <tristankopec@gmail.com>
2018-01-16 14:26:32 +0100

    Finished QRReader

---
+++ app/src/main/java/com/android/dev/qrcoursedorientation/presenters/QrPresenter.java
@@ -0,0 +1,16 @@
+package com.android.dev.qrcoursedorientation.presenters;
+
+import com.android.dev.qrcoursedorientation.views.viewsinterfaces.QrView;
+
+/**
+ * Created by iem on 16/01/2018.
+ */
+
+public class QrPresenter {
+
+    private QrView qrView;
+
+    public QrPresenter(QrView qrView) {
+        this.qrView = qrView;
+    }
+}
