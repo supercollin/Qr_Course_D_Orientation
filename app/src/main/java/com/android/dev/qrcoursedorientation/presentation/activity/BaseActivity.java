c7e35e78f35ed917e06b08d9ee2314b1bd87be1d
Tristan K <tristankopec@gmail.com>
2018-01-17 14:45:58 +0100

    Repare Merge problem

--- app/src/main/java/com/android/dev/qrcoursedorientation/presentation/activity/BaseActivity.java
+++ app/src/main/java/com/android/dev/qrcoursedorientation/presentation/activity/BaseActivity.java
@@ -1,31 +1,46 @@
 package com.android.dev.qrcoursedorientation.presentation.activity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.android.dev.qrcoursedorientation.R;
 import com.android.dev.qrcoursedorientation.presentation.fragment.QrCheckpointListFragment;
 import com.android.dev.qrcoursedorientation.presentation.fragment.QrFragment;
 import com.android.dev.qrcoursedorientation.presentation.adapter.PagerAdapter;
 import com.android.dev.qrcoursedorientation.presentation.transformers.ZoomOutPageTransformer;
+import com.android.dev.qrcoursedorientation.services.QrChronometer;
 
 import java.util.List;
 import java.util.Vector;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class BaseActivity extends FragmentActivity {
+
+public class BaseActivity extends FragmentActivity implements QrFragment.StartChronoInterface {
 
     private ViewPager viewPager;
     private PagerAdapter pagerAdapter;
+    private QrChronometer qrChronometer;
+    private boolean mServiceBound =false;
+
+    @BindView(R.id.textViewMessage) TextView headerMessage;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        ButterKnife.bind(this,this);
 
         // Création de la liste de Fragments que fera défiler le PagerAdapter
         List fragments = new Vector();
@@ -42,14 +57,63 @@ public class BaseActivity extends FragmentActivity {
     }
 
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mServiceBound) {
+            unbindService(mServiceConnection);
+            mServiceBound = false;
+        }
+    }
+    @Override
+    public void startChrono(String link) {
+        Intent intent = new Intent(this, QrChronometer.class);
+        startService(intent);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
 
-//    public void displayQRView(){
-//        qrNavigator.launchQrFragment();
-//    }
+        final Thread t = new Thread() {
 
-//    public void displayQRCheckpointListView(){
-//        qrNavigator.launchQrCheckpointListFragment();
-//    }
+            @Override
+            public void run() {
+                while (!isInterrupted()) {
+                    try {
+                        Thread.sleep(1000);  //1000ms = 1 sec
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if(qrChronometer != null){
+                                    headerMessage.setText(qrChronometer.getTimestamp());
+                                }
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        t.start();
+    }
+
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+            QrChronometer.MyBinder myBinder = (QrChronometer.MyBinder) service;
+            qrChronometer = myBinder.getService();
+            mServiceBound = true;
+        }
+    };
+
+
 
 
 
--- app/src/main/java/com/android/dev/qrcoursedorientation/presentation/fragment/QrFragment.java
+++ app/src/main/java/com/android/dev/qrcoursedorientation/presentation/fragment/QrFragment.java
@@ -7,29 +7,33 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
+import android.content.Context;
 import com.android.dev.qrcoursedorientation.presentation.viewsinterfaces.QrView;
+
 import com.google.zxing.Result;
-
-
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-/**
- * Created by iem on 16/01/2018.
- */
 
 public class QrFragment extends Fragment implements QrView, ZXingScannerView.ResultHandler{
-
     private ZXingScannerView mScannerView;
     View view;
+    private StartChronoInterface listener;
 
-    public static QrFragment newInstance() {
-        QrFragment fragment = new QrFragment();
-
-        return fragment;
+    public interface StartChronoInterface {
+        // This can be any number of events to be sent to the activity
+        void startChrono(String link);
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof StartChronoInterface) {
+        } else {
+            listener = (StartChronoInterface) context;
+            throw new ClassCastException(context.toString()
+                    + " must implement MyListFragment.OnItemSelectedListener");
+        }
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         mScannerView = new ZXingScannerView(this.getContext());   // Programmatically initialize the scanner view
@@ -37,8 +41,6 @@ public class QrFragment extends Fragment implements QrView, ZXingScannerView.Res
         mScannerView.startCamera();
         return view;
     }
-
-
 
     @Override
     public void onResume() {
@@ -52,10 +54,12 @@ public class QrFragment extends Fragment implements QrView, ZXingScannerView.Res
         super.onPause();
         mScannerView.stopCamera();           // Stop camera on pause
     }
+
     @Override
     public void handleResult(Result rawResult) {
         // Do something with the result here
         Log.v("ok", rawResult.getText()); // Prints scan results
         mScannerView.resumeCameraPreview(this);
+        listener.startChrono("start chrono");
     }
 }
