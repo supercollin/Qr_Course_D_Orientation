80d67cdcf18bd89bce89856f7f8393665f045e47
Tristan K <tristankopec@gmail.com>
2018-01-18 11:12:06 +0100

    Repaire merge

--- app/src/main/java/com/android/dev/qrcoursedorientation/presentation/fragment/QrFragment.java
+++ app/src/main/java/com/android/dev/qrcoursedorientation/presentation/fragment/QrFragment.java
@@ -1,25 +1,5 @@
+package com.android.dev.qrcoursedorientation.presentation.fragment;
 
-<<<<<<< HEAD
-        // Do something with the result here
-        Log.v("ok", rawResult.getText()); // Prints scan results
-        mScannerView.resumeCameraPreview(this);
-        if(CheckPointManager.getCheckpointList().size() == 0 && CheckPointManager.isRun()) {
-            listener.startChrono("start chrono");
-        }
-        CheckPointManager.createCheckPoint(rawResult.getText(), 0, 0);
-        Log.d("checkpoint", CheckPointManager.getCheckpointList().toString());
-    }
-}
-        // Do something with the result here
-        Log.v("ok", rawResult.getText()); // Prints scan results
-        mScannerView.resumeCameraPreview(this);
-        if(CheckPointManager.getCheckpointList().size() == 0 && CheckPointManager.isRun()) {
-            listener.startChrono("start chrono");
-        }
-        CheckPointManager.createCheckPoint(rawResult.getText(), 0, 0);
-        Log.d("checkpoint", CheckPointManager.getCheckpointList().toString());
-        //Log.d("checkpoint", CheckPointManager.getCheckpointList().toString());
-=======
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -32,51 +12,66 @@ import android.view.ViewGroup;
 import android.content.Context;
 
 import com.android.dev.qrcoursedorientation.managers.CheckPointManager;
-import com.android.dev.qrcoursedorientation.presentation.activity.BaseActivity;
 import com.android.dev.qrcoursedorientation.presentation.viewsinterfaces.QrView;
->>>>>>> PutDataIntoRecyclerView
 
-        //On envoie les données au fragment QR_CheckpointList
-        final Intent intent = new Intent("UPDATE_DATA");
-        intent.putExtra("CHECKPOINT_LIST", "update");
-        LocalBroadcastManager.getInstance(this.getContext()).sendBroadcast(intent);
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 
+public class QrFragment extends Fragment implements QrView, ZXingScannerView.ResultHandler{
+    private ZXingScannerView mScannerView;
+    View view;
+    private StartChronoInterface listener;
+
+    public interface StartChronoInterface {
+        // This can be any number of events to be sent to the activity
+        void startChrono(String link);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        listener = (StartChronoInterface) context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        mScannerView = new ZXingScannerView(this.getContext());   // Programmatically initialize the scanner view
+        view = mScannerView;                // Set the scanner view as the content view
+        mScannerView.startCamera();
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
 
 
 
-
-
-
-
-
-
-<<<<<<< HEAD
-=======
     @Override
     public void handleResult(Result rawResult) {
         // Do something with the result here
         Log.v("ok", rawResult.getText()); // Prints scan results
         mScannerView.resumeCameraPreview(this);
-        listener.startChrono("start chrono");
+        
+        if(CheckPointManager.getCheckpointList().size() == 0 && CheckPointManager.isRun()) {
+            listener.startChrono("start chrono");
+        }
         CheckPointManager.createCheckPoint(rawResult.getText(), 0, 0);
-        //Log.d("checkpoint", CheckPointManager.getCheckpointList().toString());
 
-        //On envoie les données au fragment QR_CheckpointList
         final Intent intent = new Intent("UPDATE_DATA");
         intent.putExtra("CHECKPOINT_LIST", "update");
         LocalBroadcastManager.getInstance(this.getContext()).sendBroadcast(intent);
 
-
-
-
-
-
-
-
-
-
-
->>>>>>> PutDataIntoRecyclerView
     }
 }
