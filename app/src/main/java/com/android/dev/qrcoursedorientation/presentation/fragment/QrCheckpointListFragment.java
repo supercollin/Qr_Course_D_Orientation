2db7b04d9e447b4472bb326bc2c02f83a3218aee
Tristan K <tristankopec@gmail.com>
2018-01-18 11:05:01 +0100

    Added BroadCastReceiver

--- app/src/main/java/com/android/dev/qrcoursedorientation/presentation/fragment/QrCheckpointListFragment.java
+++ app/src/main/java/com/android/dev/qrcoursedorientation/presentation/fragment/QrCheckpointListFragment.java
@@ -1,8 +1,13 @@
 package com.android.dev.qrcoursedorientation.presentation.fragment;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -11,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.dev.qrcoursedorientation.R;
+import com.android.dev.qrcoursedorientation.managers.CheckPointManager;
 import com.android.dev.qrcoursedorientation.models.Checkpoint;
 import com.android.dev.qrcoursedorientation.presentation.adapter.QrCheckpointListAdapter;
 import com.android.dev.qrcoursedorientation.presentation.presenters.QrCheckpointListPresenter;
@@ -47,11 +53,18 @@ public class QrCheckpointListFragment extends Fragment implements QrCheckpointLi
         this.view = inflater.inflate(R.layout.fragment_qr_checkpoint_list, container, false);
         ButterKnife.bind(this,view);
 
+        LocalBroadcastManager.getInstance(getContext()).registerReceiver(broadcastReceiver, new IntentFilter("UPDATE_DATA"));
 
 
         return view;
     }
 
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(getContext()).unregisterReceiver(broadcastReceiver);
+    }
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -60,12 +73,12 @@ public class QrCheckpointListFragment extends Fragment implements QrCheckpointLi
 
 
 
-        List<QrCheckpointViewModel> qrCheckpointListViewModels = new ArrayList<>();
-        qrCheckpointListViewModels.add(new QrCheckpointViewModel(new Checkpoint("1", "789", 10.0, 5.5)));
-        qrCheckpointListViewModels.add(new QrCheckpointViewModel(new Checkpoint("2", "2221", 10.0, 5.5)));
-        qrCheckpointListViewModels.add(new QrCheckpointViewModel(new Checkpoint("3", "456", 10.0, 5.5)));
-        qrCheckpointListViewModels.add(new QrCheckpointViewModel(new Checkpoint("4", "958", 10.0, 5.5)));
-        updateList(qrCheckpointListViewModels);
+//        List<QrCheckpointViewModel> qrCheckpointListViewModels = new ArrayList<>();
+//        qrCheckpointListViewModels.add(new QrCheckpointViewModel(new Checkpoint("1", "789", 10.0, 5.5)));
+//        qrCheckpointListViewModels.add(new QrCheckpointViewModel(new Checkpoint("2", "2221", 10.0, 5.5)));
+//        qrCheckpointListViewModels.add(new QrCheckpointViewModel(new Checkpoint("3", "456", 10.0, 5.5)));
+//        qrCheckpointListViewModels.add(new QrCheckpointViewModel(new Checkpoint("4", "958", 10.0, 5.5)));
+//        updateList(qrCheckpointListViewModels);
     }
 
     @Override
@@ -78,4 +91,20 @@ public class QrCheckpointListFragment extends Fragment implements QrCheckpointLi
         qrCheckpointListRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         qrCheckpointListRecyclerView.setAdapter(qrCheckpointListAdapter);
     }
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if ("UPDATE_DATA".equals(intent.getAction()) == true) {
+                List<Checkpoint> checkpointList = CheckPointManager.getCheckpointList();
+                List<QrCheckpointViewModel> qrCheckpointViewModels = new ArrayList<>();
+                for(int i=0; i < checkpointList.size(); i++){
+                    qrCheckpointViewModels.add(new QrCheckpointViewModel(checkpointList.get(i)));
+                }
+
+                updateList(qrCheckpointViewModels);
+            }
+        }
+    };
+
 }
--- app/src/main/java/com/android/dev/qrcoursedorientation/presentation/fragment/QrFragment.java
+++ app/src/main/java/com/android/dev/qrcoursedorientation/presentation/fragment/QrFragment.java
@@ -1,63 +1,4 @@
-package com.android.dev.qrcoursedorientation.presentation.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.content.Context;
-
-import com.android.dev.qrcoursedorientation.managers.CheckPointManager;
-import com.android.dev.qrcoursedorientation.presentation.viewsinterfaces.QrView;
-
-import com.google.zxing.Result;
-
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
-
-
-public class QrFragment extends Fragment implements QrView, ZXingScannerView.ResultHandler{
-    private ZXingScannerView mScannerView;
-    View view;
-    private StartChronoInterface listener;
-
-    public interface StartChronoInterface {
-        // This can be any number of events to be sent to the activity
-        void startChrono(String link);
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        listener = (StartChronoInterface) context;
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        mScannerView = new ZXingScannerView(this.getContext());   // Programmatically initialize the scanner view
-        view = mScannerView;                // Set the scanner view as the content view
-        mScannerView.startCamera();
-        return view;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
-        mScannerView.startCamera();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        mScannerView.stopCamera();           // Stop camera on pause
-    }
-
-
-
-    @Override
-    public void handleResult(Result rawResult) {
         // Do something with the result here
         Log.v("ok", rawResult.getText()); // Prints scan results
         mScannerView.resumeCameraPreview(this);
@@ -65,6 +6,33 @@ public class QrFragment extends Fragment implements QrView, ZXingScannerView.Res
             listener.startChrono("start chrono");
         }
         CheckPointManager.createCheckPoint(rawResult.getText(), 0, 0);
-        Log.d("lidt", CheckPointManager.getCheckpointList().toString());
+        Log.d("checkpoint", CheckPointManager.getCheckpointList().toString());
+    }
+}
+        // Do something with the result here
+        Log.v("ok", rawResult.getText()); // Prints scan results
+        mScannerView.resumeCameraPreview(this);
+        if(CheckPointManager.getCheckpointList().size() == 0 && CheckPointManager.isRun()) {
+            listener.startChrono("start chrono");
+        }
+        CheckPointManager.createCheckPoint(rawResult.getText(), 0, 0);
+        Log.d("checkpoint", CheckPointManager.getCheckpointList().toString());
+        //Log.d("checkpoint", CheckPointManager.getCheckpointList().toString());
+
+        //On envoie les données au fragment QR_CheckpointList
+        final Intent intent = new Intent("UPDATE_DATA");
+        intent.putExtra("CHECKPOINT_LIST", "update");
+        LocalBroadcastManager.getInstance(this.getContext()).sendBroadcast(intent);
+
+
+
+
+
+
+
+
+
+
+
     }
 }
